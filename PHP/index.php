cc0db0f6af8ba1d911edfeece14b9795330106a9
Victor-Alex-Moreira-Gouveia <vamagemes87@gmail.com>
2024-10-03 17:01:35 -0300

    Reajuste na estrutura e Correção do arquivo .sql

---
+++ PHP/index.php
@@ -0,0 +1,14 @@
+<?php
+    include('db.php');  // Inclui a conexão com o banco de dados
+
+    // Recupera todos os jogadores cadastrados
+    $sql = "SELECT * FROM jogadores";
+    $result = $conn->query($sql);
+    $jogadores = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $jogadores[] = $row;  // Adiciona cada jogador ao array
+        }
+    }
+?>
