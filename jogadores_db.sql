a6bc8f5118d3d6eb1e552f288aae98c3fbed5c93
Victor-Alex-Moreira-Gouveia <vamgames87@gmail.com>
2024-10-04 18:02:49 -0300

    Acho que ajustei o sql.

--- jogadores_db.sql
+++ jogadores_db.sql
@@ -1,51 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Tempo de geração: 03/10/2024 às 12:04
--- Versão do servidor: 8.3.0
--- Versão do PHP: 8.2.18
+-- Comentando o SELECT, pode ser útil para debug
+-- SELECT 'Banco de dados já existe vamos partir de onde começamos' FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'jogadores_db';
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `jogadores_db`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `jogadores`
---
-
-DROP TABLE IF EXISTS `jogadores`;
-CREATE TABLE IF NOT EXISTS `jogadores` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) DEFAULT NULL,
-  `sexo` varchar(1) DEFAULT NULL,
-  `escola` varchar(100) DEFAULT NULL,
-  `pontuacao` int DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Despejando dados para a tabela `jogadores`
---
-
-INSERT INTO `jogadores` (`id`, `nome`, `sexo`, `escola`, `pontuacao`) VALUES
-(1, 'Enzo Ribeiro', '', 'Unigrau', 5),
-(2, 'luis', '', 'Cândido', 5);
+SELECT 'Banco de dados já existe' FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'jogadores_db';
+CREATE DATABASE IF NOT EXISTS `jogadores_db` DEFAULT CHARACTER SET = 'utf8mb4';
+USE `jogadores_db`;
+CREATE TABLE IF NOT EXISTS `jogadores`(
+    `id` INT AUTO_INCREMENT,
+    `nome` VARCHAR(60),
+    `sexo` CHAR(1) DEFAULT NULL,
+    `escola` VARCHAR(60),
+    `pontuacao` INT DEFAULT 0,
+    PRIMARY KEY(`id`)
+);
+INSERT INTO `jogadores` (`id`, `nome`, `sexo`, `escola`, `pontuacao`) VALUES 
+(NULL, "Victor Alex Moreira Gouveia", "M", "Etec José Del Guerra", 5),
+(NULL, "Enzo Fernando", "M", "Etec José Del Guerra", 4);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
