c5865532f8ad1056254e4d4e5c34baea54a1cd7a
Victor-Alex-Moreira-Gouveia <vamgames87@gmail.com>
2024-10-04 23:25:50 -0300

    Conten anotações super importantes

---
+++ NOTA.md
@@ -0,0 +1,6 @@
+# Victor Alex Moreira Gouveia
+## jogadores_db.sql
+Não use o phpmyadmin para gerencia o banco de dados sua configurações não permite alguns comandos do sql estão bugados então usa o proprio cmd do Mysql. Para executar esse sql no cmd execute isso.
+- mysql \. /caminho/para/arquivo/jogadores-db.sql
+
+espero que vcs não tenhão configurado o arquivos de CRUD (db.php) para que funcione somente no phpmyadmin.
--- jogadores_db.sql
+++ jogadores_db.sql
@@ -1,9 +1,10 @@
 -- Comentando o SELECT, pode ser útil para debug
 -- SELECT 'Banco de dados já existe vamos partir de onde começamos' FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'jogadores_db';
-
 SELECT 'Banco de dados já existe' FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'jogadores_db';
-CREATE DATABASE IF NOT EXISTS `jogadores_db` DEFAULT CHARACTER SET = 'utf8mb4';
+
+CREATE DATABASE IF NOT EXISTS `jogadores_db` DEFAULT CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_general_ci';
 USE `jogadores_db`;
+
 CREATE TABLE IF NOT EXISTS `jogadores`(
     `id` INT AUTO_INCREMENT,
     `nome` VARCHAR(60),
@@ -12,7 +13,9 @@ CREATE TABLE IF NOT EXISTS `jogadores`(
     `pontuacao` INT DEFAULT 0,
     PRIMARY KEY(`id`)
 );
+
 INSERT INTO `jogadores` (`id`, `nome`, `sexo`, `escola`, `pontuacao`) VALUES 
 (NULL, "Victor Alex Moreira Gouveia", "M", "Etec José Del Guerra", 5),
 (NULL, "Enzo Fernando", "M", "Etec José Del Guerra", 4);
+
 COMMIT;
