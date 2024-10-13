cdbadbc89765c956c5b81e35469aaa691c0e7dbc
Enzo Ribeiro Martin <ribeiroenzo998@gmail.com>
2024-10-13 11:06:12 -0300

    colocando icone

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if ($result->num_rows > 0) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/iconeBola" type="image/png">
     <title>Contador de Detecções</title>
     <style>
     <?php include 'css/style.css'; ?>
